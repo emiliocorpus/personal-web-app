aaf5c44604c537866e9a6fcfed9462cc72e5561d
emiliocorpus <emiliocorpus@gmail.com>
2016-07-11 16:25:43 -0700

    adds basic layout of webpage

---
+++ app/assets/javascripts/jquery-click-actions.js
@@ -0,0 +1,17 @@
+$(document).on('click','.searchbychar', function(event) {
+    event.preventDefault();
+    var target = "#" + this.getAttribute('data-target');
+    $('html, body').animate({
+        scrollTop: $(target).offset().top
+    }, 300);
+});
+
+
+
+$(document).on('click','.arrow-links', function(event) {
+    event.preventDefault();
+    var target = "#" + this.getAttribute('data-target');
+    $('html, body').animate({
+        scrollTop: $(target).offset().top
+    }, 300);
+});
--- app/assets/javascripts/scroll-animation.js
+++
@@ -1,7 +0,0 @@
-$(document).on('click','.searchbychar', function(event) {
-    event.preventDefault();
-    var target = "#" + this.getAttribute('data-target');
-    $('html, body').animate({
-        scrollTop: $(target).offset().top
-    }, 300);
-});
